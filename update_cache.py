899fe379fc9f2f662a8ff0933cfc171cc17920c6
anuragpras <ianuragprasad@gmail.com>
2026-07-04 16:10:44 +0530

    Fix sticky footer, restore full width line, reduce footer height significantly

---
+++ update_cache.py
@@ -0,0 +1,20 @@
+import os, glob, re
+
+html_files = []
+for root, dirs, files in os.walk('.'):
+    if 'node_modules' in root or '.git' in root or '.vercel' in root:
+        continue
+    for file in files:
+        if file.endswith('.html'):
+            html_files.append(os.path.join(root, file))
+
+for filepath in html_files:
+    with open(filepath, 'r', encoding='utf-8') as f:
+        html = f.read()
+    
+    html = re.sub(r'href="/css/style\.css\?v=\d+"', 'href="/css/style.css?v=4"', html)
+    
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(html)
+
+print('Updated cache busters to v=4')
